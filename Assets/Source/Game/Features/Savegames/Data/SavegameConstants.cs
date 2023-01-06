8268c2e9f239e2709698470205696461e272ddae
SMaleck <s.maleck@live.de>
2023-01-06 18:08:34 +0100

    Adds savegame integration

---
+++ Assets/Source/Game/Features/Savegames/Data/SavegameConstants.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Features.Savegames.Data
+{
+    public static class SavegameConstants
+    {
+        public static string RootPath => Application.isEditor ? "Savegame" : Application.persistentDataPath;
+        public static string GlobalSaveFileName => "global.sav";
+    }
+}
---
+++ Assets/Source/Game/Features/Savegames/SavegameFeature.cs
@@ -0,0 +1,37 @@
+﻿using Game.Features.Player.Creation;
+using Game.Features.Player.Savegames;
+using Game.Features.Savegames.Data;
+using Savegames;
+using Savegames.IO;
+using Savegames.Middleware;
+using Savegames.Serialization;
+using System;
+
+namespace Game.Features.Savegames
+{
+    public class SavegameFeature : Feature
+    {
+        private readonly IStorageIO _storageIO;
+        private readonly ISerializer _serializer;
+        private readonly IMiddlewareProcessor _middleware;
+
+        public ISavegameStorage<GlobalSavegame> GlobalSavegameStorage;
+
+        public SavegameFeature()
+        {
+            _storageIO = new FileStorageIO(SavegameConstants.RootPath);
+            _serializer = new JsonSerializer();
+            _middleware = new MiddlewareProcessor(
+                Array.Empty<ISavegameObjectMiddleware>(),
+                Array.Empty<ISavegameSerializedMiddleware>());
+
+            GlobalSavegameStorage = new SavegameStorage<GlobalSavegame>(
+                _storageIO,
+                _serializer,
+                SavegameConstants.GlobalSaveFileName,
+                _middleware);
+
+            GlobalSavegameStorage.Initialize(SavegameFactory.CreateGlobalSavegame);
+        }
+    }
+}
