badf8b27dbd0b90310d9b8876159221ba5d97598
SMaleck <s.maleck@live.de>
2023-01-06 10:28:49 +0100

    Adds time service

---
+++ Assets/Source/Game/Services/Time/TimeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Game.Services.Time
+{
+    public class TimeService : Service
+    {
+        public DateTime Now => DateTime.Now;
+        public DateTime NowUtc => DateTime.UtcNow;
+    }
+}
