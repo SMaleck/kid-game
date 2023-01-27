58790c5a8776976eb6396c3b01b30896a40e4773
SMaleck <s.maleck@live.de>
2023-01-27 13:14:55 +0100

    Adds platform type

--- Assets/Source/Game/Services/ClientInfo/ClientInfoService.cs
+++ Assets/Source/Game/Services/ClientInfo/ClientInfoService.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Game.Services.ClientInfo
 {
@@ -6,6 +7,48 @@ namespace Game.Services.ClientInfo
     {
         public string Version => UnityEngine.Application.version;
         public RuntimePlatform Platform => UnityEngine.Application.platform;
+        public PlatformType PlatformType { get; }
         public bool IsDebug => UnityEngine.Debug.isDebugBuild;
+
+        public ClientInfoService()
+        {
+            PlatformType = GetPlatformType();
+        }
+
+        private PlatformType GetPlatformType()
+        {
+            switch(Platform)
+            {
+                case RuntimePlatform.OSXEditor:
+                case RuntimePlatform.WindowsEditor:
+                case RuntimePlatform.LinuxEditor:
+                    return PlatformType.Editor;
+
+                case RuntimePlatform.OSXPlayer:
+                case RuntimePlatform.WindowsPlayer:
+                case RuntimePlatform.LinuxPlayer:
+                    return PlatformType.Computer;
+                    
+                case RuntimePlatform.WebGLPlayer:
+                    return PlatformType.Browser;
+                
+                case RuntimePlatform.IPhonePlayer:
+                case RuntimePlatform.Android:
+                    return PlatformType.Mobile;
+
+                case RuntimePlatform.XBOX360:
+                case RuntimePlatform.PS3:
+                case RuntimePlatform.PS4:
+                case RuntimePlatform.XboxOne:
+                case RuntimePlatform.Switch:
+                case RuntimePlatform.GameCoreXboxSeries:
+                case RuntimePlatform.GameCoreXboxOne:
+                case RuntimePlatform.PS5:
+                    return PlatformType.Console;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
     }
 }
---
+++ Assets/Source/Game/Services/ClientInfo/PlatformType.cs
@@ -0,0 +1,11 @@
+﻿namespace Game.Services.ClientInfo
+{
+    public enum PlatformType
+    {
+        Editor,
+        Computer,
+        Console,
+        Mobile,
+        Browser
+    }
+}
