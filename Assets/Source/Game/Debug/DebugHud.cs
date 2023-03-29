241c88a8ae692eb9293e861df9151906963aa266
SMaleck <s.maleck@live.de>
2023-03-29 08:57:10 +0200

    Adds FPS display

--- Assets/Source/Game/Debug/DebugHud.cs
+++ Assets/Source/Game/Debug/DebugHud.cs
@@ -1,6 +1,7 @@
 ﻿using Game.Features.GameWorld.Levels;
 using Game.Static.Locators;
 using Game.Utility;
+using System.Text;
 using UnityEngine;
 
 namespace Game.Debug
@@ -8,6 +9,8 @@ namespace Game.Debug
     public class DebugHud : MonoBehaviour
     {
         private LevelStateFeature _levelState;
+        private readonly FpsProfiler _fpsProfiler = new FpsProfiler();
+        private readonly StringBuilder _sb = new StringBuilder();
 
         private GUIStyle _style;
         private GUIStyle Style => _style ??= CreateGUIStyle();
@@ -17,12 +20,18 @@ namespace Game.Debug
             _levelState = FeatureLocator.Get<LevelStateFeature>();
         }
 
+        private void Update()
+        {
+            _fpsProfiler.OnUpdate();
+        }
+
         private static GUIStyle CreateGUIStyle()
         {
             var style = new GUIStyle
             {
                 normal = { textColor = new Color(0.0f, 1.0f, 0.0f, 0.7f) },
-                alignment = TextAnchor.UpperLeft
+                alignment = TextAnchor.UpperLeft,
+                fontSize = 36
             };
             var h = Screen.height;
             style.fontSize = h * 2 / 100;
@@ -38,13 +47,26 @@ namespace Game.Debug
             var cornerOffset = h * 2 / 200;
             var rect = new Rect(cornerOffset, cornerOffset + safeAreaHeight, w, h * 2f / 100);
 
-            GUI.Label(rect, GetProgress(), Style);
+            GUI.Label(rect, GetDebugString(), Style);
+        }
+
+        private string GetDebugString()
+        {
+            return _sb.Clear()
+                .AppendLine(GetFPS())
+                .AppendLine(GetProgress())
+                .ToString();
         }
 
         private string GetProgress()
         {
             var progressPercent = _levelState?.RelativeProgress ?? 0;
             return $"{progressPercent:P}";
+        }
+
+        private string GetFPS()
+        {
+            return $"FPS {_fpsProfiler.CurrentFps} | AVG {_fpsProfiler.AverageFps} | MIN {_fpsProfiler.MinFps} | MAX {_fpsProfiler.MaxFps}";
         }
     }
 }
