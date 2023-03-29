30981eb8004f5fbf605b35c08442e6ebc57d97ce
SMaleck <s.maleck@live.de>
2023-03-29 10:41:19 +0200

    Disables input blocker early

--- Assets/Source/Game/Features/UI/Loading/LoadingScreenView.cs
+++ Assets/Source/Game/Features/UI/Loading/LoadingScreenView.cs
@@ -8,8 +8,10 @@ namespace Game.Features.UI.Loading
 {
     public sealed class LoadingScreenView : ScreenView
     {
+        [SerializeField] private GameObject _inputBlocker;
         [SerializeField] private Image _background;
         [SerializeField] private float _fadeDurationSeconds;
+        [SerializeField] private float _inputBlockEarlyDisableFactor;
         [SerializeField] private Ease _fadeEase;
 
         private Tween _showTween;
@@ -19,6 +21,12 @@ namespace Game.Features.UI.Loading
         private Tween HideTween => _hideTween ??= CreateHideTween();
 
         private Action _onCompleted;
+
+        private bool IsInputBlocked
+        {
+            get => _inputBlocker.activeSelf;
+            set => _inputBlocker.SetActive(value);
+        }
 
         private void OnCompleted()
         {
@@ -32,6 +40,7 @@ namespace Game.Features.UI.Loading
             if (instant)
             {
                 _background.color = SetAlpha(_background.color, 1f);
+                IsInputBlocked = true;
                 onCompleted.Invoke();
                 return;
             }
@@ -46,6 +55,7 @@ namespace Game.Features.UI.Loading
             {
                 _background.color = SetAlpha(_background.color, 0f);
                 IsVisible = false;
+                IsInputBlocked = false;
                 onCompleted.Invoke();
                 return;
             }
@@ -60,6 +70,7 @@ namespace Game.Features.UI.Loading
                 .AppendCallback(() =>
                 {
                     IsVisible = true;
+                    IsInputBlocked = true;
                     _background.color = SetAlpha(_background.color, 0f);
                 })
                 .Append(_background.DOFade(1f, _fadeDurationSeconds).SetEase(_fadeEase))
@@ -70,9 +81,11 @@ namespace Game.Features.UI.Loading
 
         private Tween CreateHideTween()
         {
+            var inputBlockerCallbackDelaySeconds = _fadeDurationSeconds * _inputBlockEarlyDisableFactor;
             return DOTween.Sequence()
                 .AppendCallback(() => _background.color = SetAlpha(_background.color, 1f))
                 .Append(_background.DOFade(0f, _fadeDurationSeconds).SetEase(_fadeEase))
+                .InsertCallback(inputBlockerCallbackDelaySeconds, () => IsInputBlocked = false)
                 .AppendCallback(() =>
                 {
                     IsVisible = false;
