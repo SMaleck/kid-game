37706915bdb87361b27104780762a90ceeb59375
SMaleck <s.maleck@live.de>
2023-03-29 15:31:04 +0200

    Adds component

---
+++ Assets/Source/Game/Features/EntiCS/Components/PlayerEffectComponent.cs
@@ -0,0 +1,12 @@
+﻿using EntiCS.Entities.Components;
+using UnityEngine;
+
+namespace Game.Features.EntiCS.Components
+{
+    public class PlayerEffectComponent : EntityComponent
+    {
+        [field: SerializeField] public ParticleSystem JumpApexPS { get; set; }
+        [field: SerializeField] public ParticleSystem JumpStartPS { get; set; }
+        [field: SerializeField] public ParticleSystem JumpEndPS { get; set; }
+    }
+}
