2bb680338dc1294df8948c53b3fcddb07c58ba8b
SMaleck <s.maleck@live.de>
2023-04-14 12:53:17 +0200

    Regenerates code

--- Assets/Source/Game/Utility/LocatorConst.cs
+++ Assets/Source/Game/Utility/LocatorConst.cs
@@ -4,7 +4,7 @@ namespace Game.Utility
 {
     public static class LocatorConst
     {
-                public const int DataCount = 6;
+        public const int DataCount = 6;
         public const int ServiceCount = 9;
         public const int FeatureCount = 17;
 
