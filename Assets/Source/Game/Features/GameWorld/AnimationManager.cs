eb2b8969b7921501f6cd054545afda6b29ec8308
SMaleck <s.maleck@live.de>
2023-04-14 12:59:55 +0200

    Adds animation pausing

---
+++ Assets/Source/Game/Features/GameWorld/AnimationManager.cs
@@ -0,0 +1,45 @@
+﻿using Game.Features.Ticking;
+using Game.Static.Locators;
+using UnityEngine;
+
+namespace Game.Features.GameWorld
+{
+    // ToDo This should be event-based, so we don't check every frame whether to pause the PS.
+    // Acceptable in this small-scale project
+    public class AnimationManager : MonoBehaviour
+    {
+        [SerializeField] private Animator _target;
+
+        private TickerFeature _tickerFeature;
+        private bool _lastPauseState;
+
+        private void Start()
+        {
+            _tickerFeature = FeatureLocator.Get<TickerFeature>();
+            _lastPauseState = _tickerFeature.SceneTicker.IsPaused;
+
+            UpdateTargetSpeed();
+        }
+
+        private void Update()
+        {
+            if (_lastPauseState != _tickerFeature.SceneTicker.IsPaused)
+            {
+                UpdateTargetSpeed();
+            }
+
+            _lastPauseState = _tickerFeature.SceneTicker.IsPaused;
+        }
+
+        private void UpdateTargetSpeed()
+        {
+            var targetSpeed = _tickerFeature.SceneTicker.IsPaused ? 0f : 1f;
+            _target.speed = targetSpeed;
+        }
+
+        private void OnValidate()
+        {
+            _target = GetComponent<Animator>();
+        }
+    }
+}
