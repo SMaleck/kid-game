575af2b405d6ea8c7f31dc807bdb1af1181bf7bf
SMaleck <s.maleck@live.de>
2023-04-28 12:13:43 +0200

    Hides quit button on browser and mobile

---
+++ Assets/Source/Game/Utility/Mono/PlatformOnly.cs
@@ -0,0 +1,32 @@
+﻿using Game.Services.ClientInfo;
+using Game.Static.Locators;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Utility.Mono
+{
+    public class PlatformOnly : MonoBehaviour
+    {
+        [SerializeField] private List<PlatformType> _platforms;
+        [SerializeField] private bool _destroy;
+
+        private void Awake()
+        {
+            var platformType = ServiceLocator.Get<ClientInfoService>().PlatformType;
+
+            var isAvailable = _platforms.Contains(platformType);
+            if (isAvailable)
+            {
+                return;
+            }
+
+            if (_destroy)
+            {
+                GameObject.Destroy(this);
+                return;
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+}
